fdd9b60bb30b07ac9d979f263172b3f9a62e2a15
Allen Kim <avalmercafe02@gmail.com>
2023-05-21 20:05:40 -0400

    sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-    
+    def create
+        user = User.create(user_params)
+        session[:user_id] = user.id 
+            if user.valid?
+                render json: user, status: :created
+            else 
+                render json: {errors: ["User information not valid"]}, status: :unprocessable_entity
+            end 
+    end 
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else 
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end 
+    end 
+    private 
+        def user_params
+            params.permit(:username, :image_url, :password, :password_confirmation)
+        end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users,:reviews,:shops, only: [:create,:show,:destroy,:update,:index]
+  resources :users,:reviews,:shops,:sessions, only: [:create,:show,:destroy,:update,:index]
   get '/shops', to: 'shops#index'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
+  post '/reviews', to: 'reviews#create'
+  get '/reviews', to: 'reviews#index'
+  get '/me', to: 'users#show'
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
