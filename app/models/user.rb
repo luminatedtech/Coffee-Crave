322c3989825c25b3c0c53d67474fb0496aee20a7
Allen Kim <avalmercafe02@gmail.com>
2023-05-24 12:58:53 -0400

    changing the association between user and reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :reviews
+    has_many :reviews, through: :shops
     has_secure_password
     validates :username, uniqueness: true, presence: true
 end 
