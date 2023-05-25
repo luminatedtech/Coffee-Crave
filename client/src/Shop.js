3fbb669e997554ae4a12749fd8ee935f8ff5894b
HeroicSam <cyuen10@gmail.com>
2023-05-24 20:45:13 -0400

    started on card + navbar formatting

--- client/src/Shop.js
+++ client/src/Shop.js
@@ -1,14 +1,15 @@
 import React from "react";
-function Shop ({name,shop,address,image_url}) {
-    console.log()
-    return (
-        <div id="shop">
-            <div> {name} </div>
-            <div> {address} </div>
-            <div><img src={image_url}/> </div>
-            <div>  </div>
-        </div>
-    )
+
+function Shop ({name,shop,address,image_url, index}) {
+  return (
+    <div className="shop">
+      <img alt="shop" src={image_url}/>
+      <div className="shopInfo">
+        <h1>{`${index + 1}. ${name}`}</h1>
+        <p>{address}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Shop
--- client/src/ShopList.js
+++ client/src/ShopList.js
@@ -1,20 +1,23 @@
 import React, { useState,useEffect } from "react";
 import Shop from "./Shop"
+
 function ShopList () {
-const [shops, setShops] = useState([])
 
-useEffect(() => {
+  const [shops, setShops] = useState([])
+
+  useEffect(() => {
     fetch("/shops")
-    .then((r)=> r.json())
-    .then((shops)=> setShops(shops))
-}, [])
-return (
-    <div id="shopList"> {shops.map((shop)=>(
-        <Shop key={shop.id} name={shop.name} image_url={shop.image_url} shop={shop} address={shop.address} />
-    ))}
-    </div>
-)
-}
+      .then((r)=> r.json())
+      .then((shops)=> setShops(shops))
+  }, [])
 
+  return (
+    <div className="shopList"> 
+      {shops.map((shop, i)=>(
+        <Shop index={i} key={shop.id} name={shop.name} image_url={shop.image_url} shop={shop} address={shop.address} />
+      ))}
+    </div>
+  )
+}
 
 export default ShopList
